8c5ab98c9b507fa7f31ffdea261eadba00259c82
chvxt3r <chuxtr76@proton.me>
2025-07-13 20:27:37 -0700

    Added Reporting functionality

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ def main():
     text = get_book_text(book_path)
     num_words = wordcount(text)
     chars_dict = charstats(text)
-    print(f"{num_words} words found in the documents")
-    print(chars_dict)
+    sorted_chars = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, sorted_chars)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+def print_report(book_path, num_words, sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
-
-
